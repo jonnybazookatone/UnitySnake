dea7d90b1dad22f338c7d5031392adddd442955b
Jonny Elliott <jonnynelliott@googlemail.com>
2014-03-15 22:22:47 +0100

    First commit of the scripts that manages the snake pieces

---
+++ SnakePiece.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script is attached to each snake piece, i.e., to the Snake Prefab.
+/// It will be used to update the position of the snake, in theory utilising the input from the game controller
+/// 
+/// This script accesses the SnakeManager script SnakeScript
+/// </summary>
+
+public class SnakePiece : MonoBehaviour {
+
+	// Variables start__________________
+
+	// Quick references
+	private int snakeNumber;
+	private Transform myTransform;
+
+	// This variable states if there has been a collision
+	public bool iAteFood = false;
+
+	// Variables end____________________
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Simple transform setting
+		myTransform = transform;
+
+		// Set the ID number of this snake head using the snake manager
+		GameObject snakeManager = GameObject.Find("SnakeManager");
+		SnakeScript sScript = snakeManager.GetComponent<SnakeScript>();
+		sScript.numberOfSnakes += 1;
+		snakeNumber  = sScript.numberOfSnakes;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// If the snake piece is the snake head, then check if it collided with a piece of food
+		if(snakeNumber == 1)
+		{
+			// Check if I interact with an object that has a tag of "food"
+		}
+	}
+}
---
+++ SnakeScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script will determine create and manage each piece of the snake.
+/// It will also contain the positions that each of the snakes must take utilising an array.
+/// The script will be attached to the SnakeManager GameObject in the Inspector.
+/// 
+/// This script is accessed by the SnakePiece script
+/// 
+/// Jonny Elliott 2014
+/// </summary>
+
+public class SnakeScript : MonoBehaviour {
+
+	// Variables start_____________________
+	public int numberOfSnakes = 0;
+
+
+	// Variables end_______________________
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
